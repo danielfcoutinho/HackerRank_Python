69ac662d4aa8584f27740526e2e75d779ce3e815
Daniel Coutinho <100314851+danielfcoutinho@users.noreply.github.com>
2023-11-23 19:40:27 -0300

    Create PythonDivision.py

---
+++ Introduction/PythonDivision.py
@@ -0,0 +1,15 @@
+"""
+Title     : Python: Division
+Subdomain : Introduction
+Domain    : Python
+Author    : Daniel Coutinho
+Created   : 23 Nov 2023
+Problem   : https://www.hackerrank.com/challenges/python-division/problem
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+print(a//b)
+print(a/b)
