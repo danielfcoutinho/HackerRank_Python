000c51296c33d80d08634ce6b58e7e7f92225086
Daniel Coutinho <100314851+danielfcoutinho@users.noreply.github.com>
2023-11-23 19:50:04 -0300

    Create Loops.py

---
+++ Introduction/Loops.py
@@ -0,0 +1,15 @@
+"""
+Title     : Loops
+Subdomain : Introduction
+Domain    : Python
+Author    : Daniel Coutinho
+Created   : 23 Nov 2023
+Problem   : https://www.hackerrank.com/challenges/python-loops/problem
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    
+for x in range(n):
+    res = x**2
+    print(res)
