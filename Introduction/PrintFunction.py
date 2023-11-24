7aee2220317b5cec8fe6bd5ac9e2da839d171d4b
Daniel Coutinho <100314851+danielfcoutinho@users.noreply.github.com>
2023-11-24 18:32:51 -0300

    Create PrintFunction.py

---
+++ Introduction/PrintFunction.py
@@ -0,0 +1,16 @@
+"""
+Title     : Print Function
+Subdomain : Introduction
+Domain    : Python
+Author    : Daniel Coutinho
+Created   : 24 Nov 2023
+Problem   : https://www.hackerrank.com/challenges/python-print/problem
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for x in range(n):
+        print(x+1, end='')
+        
+        
