9c059e2e1024ae6ad207a02906d6cd767edb0754
Daniel Coutinho <100314851+danielfcoutinho@users.noreply.github.com>
2023-11-24 19:13:40 -0300

    Create HelloWorld.py

---
+++ Introduction/HelloWorld.py
@@ -0,0 +1,11 @@
+"""
+Title     : Say "Hello, World!" With Python
+Subdomain : Introduction
+Domain    : Python
+Author    : Daniel Coutinho
+Created   : 23 Nov 2023
+Problem   : https://www.hackerrank.com/challenges/py-hello-world/problem
+"""
+
+testString = 'Hello, World!'
+print(testString)
