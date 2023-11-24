1c9836297bbb3ceb0de68f8fe2f232854eff02ce
Daniel Coutinho <100314851+danielfcoutinho@users.noreply.github.com>
2023-11-24 19:12:11 -0300

    Create ArithmeticOperators.py

---
+++ Introduction/ArithmeticOperators.py
@@ -0,0 +1,18 @@
+"""
+Title     : Arithmetic Operators
+Subdomain : Introduction
+Domain    : Python
+Author    : Daniel Coutinho
+Created   : 24 Nov 2023
+Problem   : https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    
+    
